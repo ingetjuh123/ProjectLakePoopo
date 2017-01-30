5b8e61a6a4e7a279ce53330cb4a07e2526da72c5
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-30 18:25:17 +0100

    update main.Sh
    involve untar in bash

--- ProjectLakePoopo/main.sh
+++ ProjectLakePoopo/main.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 python ./Python/downloadLandsatDirect.py
+cd ./data
 
-Rscript ./R/untar.R
+
+
+
+#for file in LC8*.tar.bz
+#	do
+#	bunzip2 $file
+#done
+
+DIR="./data"
+
+for file2 in LC8*.tar
+	do
+	NEWDIR=`echo $file2 | tr -d [a-zA-Z.]`
+	sudo mkdir $DIR/$NEWDIR
+	tar -xvzf $file2 -C $DIR/$NEWDIR
+done
+
+## Rscript ./R/untar.R
 
