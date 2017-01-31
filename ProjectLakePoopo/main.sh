0202f3b2bab9080195b794ee44eb7e2a2b9a06ae
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-31 10:42:19 +0100

    Working bash untar command

--- ProjectLakePoopo/main.sh
+++ ProjectLakePoopo/main.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-python ./Python/downloadLandsatDirect.py
+#python ./Python/downloadLandsatDirect.py
+
 cd ./data
 
-
-
-
 #for file in LC8*.tar.bz
-#	do
-#	bunzip2 $file
+	#do
+	#bunzip2 $file
 #done
-
-DIR="./data"
 
 for file2 in LC8*.tar
 	do
 	NEWDIR=`echo $file2 | tr -d [a-zA-Z.]`
-	sudo mkdir $DIR/$NEWDIR
-	tar -xvzf $file2 -C $DIR/$NEWDIR
+	sudo mkdir $NEWDIR
+	sudo tar -xvf $file2 -C $NEWDIR
 done
 
 ## Rscript ./R/untar.R
