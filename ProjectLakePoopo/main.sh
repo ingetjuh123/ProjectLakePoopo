b9fb09db5571f1e18e92c5d39870999c47db7a47
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-31 12:17:47 +0100

    Altered structure -Use bash for untar -Use R for calculation

--- ProjectLakePoopo/main.sh
+++ ProjectLakePoopo/main.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-#python ./Python/downloadLandsatDirect.py
-
-cd ./data
-
-#for file in LC8*.tar.bz
-	#do
-	#bunzip2 $file
-#done
-
-for file2 in LC8*.tar
-	do
-	NEWDIR=`echo $file2 | tr -d [a-zA-Z.]`
-	sudo mkdir $NEWDIR
-	sudo tar -xvf $file2 -C $NEWDIR
-done
-
-## Rscript ./R/untar.R
+python ./Python/downloadLandsatDirect.py
+bash ./Bash/untar.sh
+Rscript ./R/Calculation.R
+#Rscript ./R/Visualization.R
 
